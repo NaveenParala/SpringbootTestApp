d3c8ff7b33c5131f40022e005d931004332b6f44
NAVEENKUMAR <NAVEENKUMAR@LAPTOP-UBQO8BII>
2021-01-02 12:46:31 +0530

    WH-966656-NAVEEN FILE UPDATE

---
+++ src/main/java/com/app/sancus/test/runner/TestRunner.java
@@ -0,0 +1,15 @@
+package com.app.sancus.test.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class TestRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("WELCOME TO TEST APP ");
+
+	}
+
+}
