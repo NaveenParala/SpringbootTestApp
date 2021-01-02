12e7f3cbf42360a77c2ca4caa3048c18573645fc
NAVEENKUMAR <NAVEENKUMAR@LAPTOP-UBQO8BII>
2021-01-02 13:35:17 +0530

    WH-966656 update with slf4j logger

--- src/main/java/com/app/sancus/test/runner/TestRunner.java
+++ src/main/java/com/app/sancus/test/runner/TestRunner.java
@@ -1,14 +1,23 @@
 package com.app.sancus.test.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 @Component
 public class TestRunner implements CommandLineRunner {
 
+	private static Logger log=LoggerFactory.getLogger(TestRunner.class);
 	@Override
 	public void run(String... args) throws Exception {
+		try {
+			log.info("STARTED");
+		} catch (Exception e) {
+		e.printStackTrace();
+		}
 		int a=10;
 		System.out.println("WELCOME TO TEST APP "+a);
+		log.info("END");
 	}
 
 }
